e16de115d8e95ad36ce6ed618cbe02bc009f084b
isvk <info@isvk.ru>
2020-10-03 23:17:44 +0300

    ref Image :recycle: :busts_in_silhouette:

--- src/components/Image/Image.tsx
+++ src/components/Image/Image.tsx
@@ -1,4 +1,4 @@
-import React, { SyntheticEvent } from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 
 interface IImageProps {
@@ -7,34 +7,42 @@ interface IImageProps {
     className?: string;
 }
 
+export enum imageStatus {
+    notLoaded,
+    loaded,
+    errorServer,
+}
+
 export default function Image(props: IImageProps) {
-    const handleLoad = (e: SyntheticEvent<EventTarget>): void => {
-        let target = e.target as HTMLImageElement;
-        target.style.background = "#ffffff";
-    };
+    const [status, setStatus] = useState(imageStatus.notLoaded);
 
-    const handleError = (e: SyntheticEvent<EventTarget>): void => {
-        let target = e.target as HTMLImageElement;
-        target.style.background = "#ffffff";
-        target.src = process.env.PUBLIC_URL + "/notfound.svg";
-    };
-
-    const getUrlImg = (url: string | undefined) => {
-        if (url && url.length) return (process.env.REACT_APP_API_URL ? process.env.REACT_APP_API_URL : "/api") + url;
+    const getUrlImg = (url: string | undefined, status: imageStatus) => {
+        if (url && url.length && status !== imageStatus.errorServer)
+            return (process.env.REACT_APP_API_URL ? process.env.REACT_APP_API_URL : "/api") + url;
         else return process.env.PUBLIC_URL + "/notfound.svg";
     };
 
     return (
         <Wrapper
             className={props.className}
-            src={getUrlImg(props.url)}
+            src={getUrlImg(props.url, status)}
             alt={props.alt}
-            onLoad={handleLoad}
-            onError={handleError}
+            status={status}
+            onLoad={() => setStatus(status === imageStatus.errorServer ? imageStatus.errorServer : imageStatus.loaded)}
+            onError={() => setStatus(imageStatus.errorServer)}
         ></Wrapper>
     );
 }
 
 const Wrapper = styled.img`
-    background: url(${process.env.PUBLIC_URL + "preloader.svg"}) no-repeat center;
+    background: ${(props: { status: imageStatus }) => {
+        switch (props.status) {
+            case imageStatus.notLoaded:
+                return "url(" + process.env.PUBLIC_URL + "preloader.svg) no-repeat center";
+            case imageStatus.loaded:
+            case imageStatus.errorServer:
+            default:
+                return "#ffffff";
+        }
+    }};
 `;
