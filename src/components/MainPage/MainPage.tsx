f41297ffa6c7cba554b3023600ca0428f37c2d08
isvk <info@isvk.ru>
2020-10-10 22:38:35 +0300

    ref delete Load :recycle: :busts_in_silhouette:

--- src/components/MainPage/MainPage.tsx
+++ src/components/MainPage/MainPage.tsx
@@ -1,9 +1,10 @@
-import React from "react";
+import React, { useEffect } from "react";
 import styled from "styled-components";
 import useCustomSelector from "src/hooks/useCustomSelector";
+import useCustomDispatch from "src/hooks/useCustomDispatch";
 import { mainGetStatus } from "src/store/rootSelector";
 import { loadStatus } from "src/store/loadStatus";
-import Load from "src/components/Load/Load";
+import { loadAll } from "src/store/main/actions";
 import Preload from "src/components/Preload/Preload";
 import Warning from "src/components/Warning/Warning";
 import Head from "src/components/Head/Head";
@@ -12,11 +13,17 @@ import ListProducts from "src/components/ListProducts/ListProducts";
 
 export default function MainPage() {
     const status = useCustomSelector(mainGetStatus);
+    const dispatch = useCustomDispatch();
+
+    useEffect(() => {
+        dispatch(loadAll());
+        // eslint-disable-next-line
+    }, []);
 
     return (
         <Wrapper>
-            {status === loadStatus.notLoaded && <Load />}
-            {status !== loadStatus.loaded && status !== loadStatus.errorServer && <Preload text="Загрузка..." />}
+            {status === loadStatus.notLoaded && <Preload text="Загрузка..." />}
+            {status === loadStatus.errorServer && <Warning text="Ошибка сервера" />}
             {status === loadStatus.loaded && (
                 <>
                     <Head />
@@ -26,14 +33,13 @@ export default function MainPage() {
                     </main>
                 </>
             )}
-            {status === loadStatus.errorServer && <Warning text="Ошибка сервера" />}
         </Wrapper>
     );
 }
 
 const Wrapper = styled.div`
     max-width: 935px;
-    margin: 0px auto;
+    margin: 0 auto;
     font-size: 16px;
     background-color: #fafafa;
 
