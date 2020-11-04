598a218d5303c1df2416be53e3748307140f29b2
isvk <info@isvk.ru>
2020-11-04 17:07:51 +0300

    build 1.0.1

---
+++ build/precache-manifest.a084044b64089b82a550e6becfcf56a6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b0fab14cd36761fa31406bd29a053e9b",
+    "url": "/plugin-collections/build/index.html"
+  },
+  {
+    "revision": "98d014e86b959bb7c0bc",
+    "url": "/plugin-collections/build/static/css/2.117ce29f.chunk.css"
+  },
+  {
+    "revision": "40ebe53e48dc60e37ce0",
+    "url": "/plugin-collections/build/static/css/main.88824ec2.chunk.css"
+  },
+  {
+    "revision": "98d014e86b959bb7c0bc",
+    "url": "/plugin-collections/build/static/js/2.0c91a89c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/plugin-collections/build/static/js/2.0c91a89c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "40ebe53e48dc60e37ce0",
+    "url": "/plugin-collections/build/static/js/main.94fd9e2a.chunk.js"
+  },
+  {
+    "revision": "0740961961dc65691e50",
+    "url": "/plugin-collections/build/static/js/runtime-main.ad9fdb43.js"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.9e9cdcd895745b78fa0e04600eeeb10b.js"
+  "/plugin-collections/build/precache-manifest.a084044b64089b82a550e6becfcf56a6.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/plugin-collections/build/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
